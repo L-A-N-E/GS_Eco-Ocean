19d6d083fffb10f31b3c5518f966a670c2598c6b
Alice <alicesb2006@gmail.com>
2024-05-28 14:07:20 -0300

    Criando uma animação para o scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,46 @@
 // Abrir Menu
 function openMenu(){
-    const open = document.querySelector('#nav-mobile')
-    const menu = document.querySelector('#abrir-menu')
+    const open = document.querySelector('#nav-mobile');
+    const menu = document.querySelector('#abrir-menu');
 
-    open.style.display = 'flex'
-    menu.style.display = 'none'
+    open.style.display = 'flex';
+    menu.style.display = 'none';
 }
 
 // Fechar Menhu
 function closeMenu(){
-    const close = document.querySelector('#nav-mobile')
-    const menu = document.querySelector('#abrir-menu')
+    const close = document.querySelector('#nav-mobile');
+    const menu = document.querySelector('#abrir-menu');
 
-    close.style.display = 'none'
-    menu.style.display = 'flex'
+    close.style.display = 'none';
+    menu.style.display = 'flex';
 }
 
 // Abrir Modal
 function openModal(){
-    const modal = document.querySelector('#modal-sobre')
-    const conteudo_sobre = document.querySelector('#conteudo-sobre')
+    const modal = document.querySelector('#modal-sobre');
+    const conteudo_sobre = document.querySelector('#conteudo-sobre');
 
-    modal.style.display = 'flex'
-    conteudo_sobre.style.display = 'none'
+    modal.style.display = 'flex';
+    conteudo_sobre.style.display = 'none';
 }
 // Fechar Modal
 function closeModal(){
-    const modal = document.querySelector('#modal-sobre')
-    const conteudo_sobre = document.querySelector('#conteudo-sobre')
+    const modal = document.querySelector('#modal-sobre');
+    const conteudo_sobre = document.querySelector('#conteudo-sobre');
 
-    modal.style.display = 'none'
-    conteudo_sobre.style.display = 'flex'
+    modal.style.display = 'none';
+    conteudo_sobre.style.display = 'flex';
 }
+
+//Animação aparecer
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    })
+})
+const hiddenElemente = document.querySelectorAll(".hidden");
+hiddenElemente.forEach((el)=> observer.observe(el));
